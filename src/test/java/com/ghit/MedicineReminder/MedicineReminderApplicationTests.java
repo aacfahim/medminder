e3558980f4f8565ef77ef1faee52b47d3a47f92a
Fahim <ashfaq.itsd@goldenharvestbd.com>
2024-08-03 14:07:06 +0600

    initial commit with database setup

---
+++ src/test/java/com/ghit/MedicineReminder/MedicineReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghit.MedicineReminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
